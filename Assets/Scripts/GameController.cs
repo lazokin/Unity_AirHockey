0f3746122768e6e3d69c1f6a623e1f8dcf8821a7
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-14 12:29:17 +1100

    Add Game and Player controllers

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public PlayerController player1;
+    public PlayerController player2;
+    public Text playerScore1;
+    public Text playerScore2;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool touching;
+    private int fingerId;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (!touching && Input.touchCount > 0)
+        {
+            Touch[] touches = Input.touches;
+            foreach (Touch touch in touches)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
+                if (hit)
+                {
+                    if (hit.collider.gameObject == this.gameObject)
+                    {
+                        touching = true;
+                        fingerId = touch.fingerId;
+                    }
+                }
+            }
+        }
+
+        if (touching)
+        {
+            Touch touch = Input.GetTouch(fingerId);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                touching = false;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 screenPoint = new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane);
+                transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+            }
+        }
+
+    }
+
+}
